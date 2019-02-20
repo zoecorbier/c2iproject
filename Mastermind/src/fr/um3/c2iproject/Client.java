2271b017030e00e9fc367f0dda2ecba972585098
LoanGhd <lolo-34@live.fr>
2019-02-20 03:45:05 -0500

    Sockets

--- Mastermind/src/fr/um3/c2iproject/Client.java
+++ Mastermind/src/fr/um3/c2iproject/Client.java
@@ -1,26 +1,19 @@
 package fr.um3.c2iproject;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 
 public class Client {
 
-	public static void main(String[] zero) {
+	public static void main(String[] zero){
 		
 		Socket socket;
 		try {
-		
-		     socket = new Socket(InetAddress.getLocalHost(),2009);	
-	             socket.close();
-
-		}catch (UnknownHostException e) {
-			
-			e.printStackTrace();
-		}catch (IOException e) {
+		socket = new Socket("localhost",2009);
+		socket.close();
+		} catch (IOException e) {
 			
 			e.printStackTrace();
 		}
 	}
-
 }
+
--- Mastermind/src/fr/um3/c2iproject/Serveur.java
+++ Mastermind/src/fr/um3/c2iproject/Serveur.java
@@ -1,26 +1,49 @@
 package fr.um3.c2iproject;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
+import java.net.*;
 
 public class Serveur {
-	public static void main(String[] zero) {
-		
-		ServerSocket socketserver  ;
-		Socket socketduserveur ;
 
+	public static void main(String[] zero){
+		
+		ServerSocket socket;
 		try {
+		socket = new ServerSocket(2009);
+		Thread t = new Thread(new Accepter_clients(socket));
+		t.start();
+		System.out.println("Mes employeurs sont prêts !");
 		
-			socketserver = new ServerSocket(2009);
-			socketduserveur = socketserver.accept(); 
-			System.out.println("Un zÃ©ro s'est connectÃ© !");
-		        socketserver.close();
-		        socketduserveur.close();
-
-		}catch (IOException e) {
+		} catch (IOException e) {
+			
 			e.printStackTrace();
 		}
 	}
-
 }
+
+class Accepter_clients implements Runnable {
+
+	   private ServerSocket socketserver;
+	   private Socket socket;
+	   private int nbrclient = 1;
+		public Accepter_clients(ServerSocket s){
+			socketserver = s;
+		}
+		
+		public void run() {
+
+	        try {
+	        	while(true){
+			  socket = socketserver.accept(); // Un client se connecte on l'accepte
+	                  System.out.println("Le client numéro "+nbrclient+ " est connecté !");
+	                  nbrclient++;
+	                  socket.close();
+	        	}
+	        
+	        } catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+
