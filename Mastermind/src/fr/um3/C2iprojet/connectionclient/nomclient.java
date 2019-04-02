c0a13b3a9c252f0f2cf7758cd1590c3277672859
zoecorbier <zoecorbier@192.168.77.169>
2019-04-02 15:55:29 +0200

    ok

--- Mastermind/src/fr/um3/C2iprojet/connectionclient/nomclient.java
+++ Mastermind/src/fr/um3/C2iprojet/connectionclient/nomclient.java
@@ -1,19 +1,30 @@
 package fr.um3.C2iprojet.connectionclient;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.List;
 
 
 
 public class nomclient {
 	private List<Nom> Noms =new ArrayList<Nom>();
-	private Nom e= new Nom();
+	private Nom e= new Nom();// prend en compte un nom rentré par un client 
 	
 
 	public void Nomliste(Nom e) {
-		for (int i=0;i <= Noms.size();i++){
-			if (Noms.get(i)==e) {
-				i= Noms.size()+1;}
+		for (int i=0;i <= Noms.size();i++){//parsours la liste des noms déjà utlisé 
+			if (Noms.get(i)==e) {//vérifie si le nomexiste déjà
+				Scanner s = new Scanner(System.in);
+				System.out.println("Avez-vous déjà joué?");
+				String n=s.nextLine();
+				if (n=="oui") {
+					i= Noms.size()+1;}
+				else {
+					Scanner k= new Scanner (System.in);
+					System.out.println("Veuillez saisir un autre nom, ce nom est déjà utilisé");
+					String e=s.nextLine();
+				}
+				}
 			else if(i== Noms.size()){
 				Noms.add(e);
 				
