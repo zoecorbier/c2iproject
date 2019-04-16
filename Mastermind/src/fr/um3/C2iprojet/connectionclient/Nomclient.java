b4392de787f41745989e4aa7c9f3887abb4b3ce0
zoecorbier <zoecorbier@172.20.10.10>
2019-04-16 15:08:18 +0200

    ok

--- Mastermind/src/fr/um3/C2iprojet/connectionclient/Nomclient.java
+++ Mastermind/src/fr/um3/C2iprojet/connectionclient/Nomclient.java
@@ -7,86 +7,46 @@ import java.util.List;
 
 
 public class Nomclient {
-		//private List<Nom> Noms =new ArrayList<Nom>();
-		/*private Nom e= new Nom();*/
-		// prend en compte un nom rentr√© par un client 
-		
+		private String NomJoueur;
 		private List<String> Noms = new ArrayList<String>();
-		
-		
 		public List<String> getNoms() {
 			return Noms;
 		}
-
-
 		public void setNoms(List<String> noms) {
 			Noms = noms;
 		}
-
-
 		public String getNomJoueur() {
 			return NomJoueur;
 		}
-
-
 		public void setNomJoueur(String nomJoueur) {
 			NomJoueur = nomJoueur;
 		}
-
-
-		private String NomJoueur;
-		
-		
-		
-		
 		public void NomListe(Scanner s ) {
-			
-			
 			System.out.println("Veuillez saisir votre nom:");
 			String c= s.nextLine();
-			
 			int i=0;
 			boolean var=false;
-			
 			while((i<Noms.size()) && (var==false)) {
-				
-				
 				if(Noms.get(i).equals(c)) {
-					System.out.println("Avez-vous dÈj‡ jouÈ ?");
+					System.out.println("Avez-vous deja joue ?");
 					c=s.nextLine();
-					
 					if(c.equals("oui")) {
-						
 						NomJoueur=Noms.get(i);
 						var=true;
-						System.out.println("Nom saisis avec succËs");
-						
-						
-					}
+						System.out.println("Nom saisis avec succes");
+						}
 					else {
-						
-						System.out.println("Ce nom est dÈj‡ utilisÈ ;(");
+						System.out.println("Ce nom est deja utilise ;(");
 						this.NomListe(s);
-						
+						}
 					}
-					
-				}
-				
-				
-				
-				else{
-					
-					
-					i++;
-					
-					
-				}
-			}
-			
+			else{
+				i++;
+					}}
 			if(i==Noms.size()) {
 				Noms.add(c);
 				NomJoueur=c;
-				System.out.println("Nom saisis avec succËs");
+				System.out.println("Nom saisis avec succes");
 			}
 			
 			
